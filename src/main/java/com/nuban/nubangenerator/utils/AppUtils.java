1ac8f4d3ab3beaeb97685b01a2f558058dd38f54
okorojeremiah <okorojeremiah91@gmail.com>
2023-05-20 12:18:39 +0100

    created AppUtils

---
+++ src/main/java/com/nuban/nubangenerator/utils/AppUtils.java
@@ -0,0 +1,8 @@
+package com.nuban.nubangenerator.utils;
+
+public class AppUtils {
+    public static final String seed = "373373373373";
+    public static final int nubanLength = 10;
+    public static final int serialNumLength = 9;
+    public static final int bankCodeLength = 3;
+}
